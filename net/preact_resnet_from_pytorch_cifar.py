f62852bbb44134aa4df777b5d04bd204eeecd229
SergeM <sbmatyunin@gmail.com>
2021-01-17 20:48:00 +0100

    customizable number of planes in preact resnet, adaptive average pool

--- net/preact_resnet_from_pytorch_cifar.py
+++ net/preact_resnet_from_pytorch_cifar.py
@@ -65,16 +65,16 @@ class PreActBottleneck(nn.Module):
 
 
 class PreActResNet(nn.Module):
-    def __init__(self, block, num_blocks, num_classes=10):
+    def __init__(self, block, num_blocks, num_classes=10, num_planes=(64, 128, 256, 512)):
         super(PreActResNet, self).__init__()
         self.in_planes = 64
 
         self.conv1 = nn.Conv2d(3, 64, kernel_size=3, stride=1, padding=1, bias=False)
-        self.layer1 = self._make_layer(block, 64, num_blocks[0], stride=1)
-        self.layer2 = self._make_layer(block, 128, num_blocks[1], stride=2)
-        self.layer3 = self._make_layer(block, 256, num_blocks[2], stride=2)
-        self.layer4 = self._make_layer(block, 512, num_blocks[3], stride=2)
-        self.linear = nn.Linear(512 * block.expansion, num_classes)
+        self.layer1 = self._make_layer(block, num_planes[0], num_blocks[0], stride=1)
+        self.layer2 = self._make_layer(block, num_planes[1], num_blocks[1], stride=2)
+        self.layer3 = self._make_layer(block, num_planes[2], num_blocks[2], stride=2)
+        self.layer4 = self._make_layer(block, num_planes[3], num_blocks[3], stride=2)
+        self.linear = nn.Linear(num_planes[3] * block.expansion, num_classes)
 
     def _make_layer(self, block, planes, num_blocks, stride):
         strides = [stride] + [1] * (num_blocks - 1)
@@ -90,7 +90,7 @@ class PreActResNet(nn.Module):
         out = self.layer2(out)
         out = self.layer3(out)
         out = self.layer4(out)
-        out = F.avg_pool2d(out, 4)
+        out = F.adaptive_avg_pool2d(out, 1)
         out = out.view(out.size(0), -1)
         out = self.linear(out)
         return out
